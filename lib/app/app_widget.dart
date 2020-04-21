54f84d4814858aa082ed3b0473bb0f2d89300560
Manoel Ribeiro <mano12328@hotmail.com>
2020-04-21 11:18:42 -0300

    Blocking the orientation to portrait

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatelessWidget {  
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+      ]);
     return MaterialApp(
       title: 'Flutter Slidy',
       theme: ThemeData(
